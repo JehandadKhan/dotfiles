43d90e924b7e5250c1488e5f47c7914250bd7af6
Jehandad Khan <jahandad@gmail.com>
2018-10-01 10:26:39 -0500

    changed find for mac compatibility

--- mk_lnks.sh
+++ mk_lnks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in $(find . -maxdepth 1 ! -name '*.swp' ! -name '.gitignore' ! -name '.git' ! -name '.' -name '.*' -printf '%P\n'); do
+for f in $(find . -maxdepth 1 ! -name '*.swp' ! -name '.gitignore' ! -name '.git' ! -name '.' -name '.*' -exec basename {} \;); do
 		echo "ln -s $(pwd)/$f  /home/$USER/$f  "
 		rm -rf /home/$USER/$f
 		ln -s $(pwd)/$f  /home/$USER/$f
