a2682dd92381e44da60078da4dfaf462e9a6574c
Jehandad Khan <jahandad@gmail.com>
2019-08-04 05:30:58 +0000

    fixed mk_links script

--- mk_lnks.sh
+++ mk_lnks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install sudo
 for f in $(find . -maxdepth 1 ! -name '*.swp' ! -name '.gitignore' ! -name '.git' ! -name '.' -name '.*' -exec basename {} \;); do
 		echo "ln -s $(pwd)/$f  $HOME/$f  "
 		rm -rf $HOME/$f
@@ -7,12 +8,15 @@ done
 
 echo "Cloning Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installing trash-cli"
 sudo apt install trash-cli
+echo "Installing exuberant-ctags"
 sudo apt install exuberant-ctags
 # install code query
+echo "Installing CodeQuery (No GUI)"
 sudo apt-get install g++ git cmake sqlite3 libsqlite3-dev qt4-dev-tools cscope exuberant-ctags
 git clone https://github.com/ruben2020/codequery.git /tmp/codequery/
-mkidir /tmp/codequery/build
+mkdir /tmp/codequery/build
 cd /tmp/codequery/build
 cmake -G "Unix Makefiles" -DNO_GUI=ON ..
 sudo make -j install 
