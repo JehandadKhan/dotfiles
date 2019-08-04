7f591e4662d2415bf7d835fd75421e306ea526a5
Jehandad Khan <jahandad@gmail.com>
2019-08-04 00:24:49 -0500

    install codequery

--- mk_lnks.sh
+++ mk_lnks.sh
@@ -9,3 +9,10 @@ echo "Cloning Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt install trash-cli
 sudo apt install exuberant-ctags
+# install code query
+sudo apt-get install g++ git cmake sqlite3 libsqlite3-dev qt4-dev-tools cscope exuberant-ctags
+git clone https://github.com/ruben2020/codequery.git /tmp/codequery/
+mkidir /tmp/codequery/build
+cd /tmp/codequery/build
+cmake -G "Unix Makefiles" -DNO_GUI=ON ..
+sudo make -j install 
