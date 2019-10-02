ace7e4b334703aca7220aec2afb341fab408a3d7
Jehandad Khan <jahandad@gmail.com>
2019-10-02 15:11:12 -0500

    skip apt confirmation

--- mk_lnks.sh
+++ mk_lnks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install vim
+sudo apt -y install vim
 for f in $(find . -maxdepth 1 ! -name '*.swp' ! -name '.gitignore' ! -name '.git' ! -name '.' -name '.*' -exec basename {} \;); do
 		echo "ln -s $(pwd)/$f  $HOME/$f  "
 		rm -rf $HOME/$f
@@ -10,12 +10,12 @@ done
 echo "Cloning Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing trash-cli"
-sudo apt install trash-cli
+sudo apt -y install trash-cli
 echo "Installing exuberant-ctags"
-sudo apt install exuberant-ctags
+sudo apt -y install exuberant-ctags
 # install code query
 echo "Installing CodeQuery (No GUI)"
-sudo apt-get install g++ git cmake sqlite3 libsqlite3-dev qt4-dev-tools cscope exuberant-ctags
+sudo apt-get -y install g++ git cmake sqlite3 libsqlite3-dev qt4-dev-tools cscope exuberant-ctags
 git clone https://github.com/ruben2020/codequery.git /tmp/codequery/
 mkdir /tmp/codequery/build
 cd /tmp/codequery/build
