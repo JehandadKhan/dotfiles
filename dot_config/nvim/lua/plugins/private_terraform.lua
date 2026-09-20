ddaf7a5540ef757d35906aceee27906e86fd6b9c
Jehandad Khan <jahandad@gmail.com>
2026-09-20 00:23:13 -0500

    fix gh helper, updated lazy vim repo names

--- dot_config/nvim/lua/plugins/private_terraform.lua
+++ dot_config/nvim/lua/plugins/private_terraform.lua
@@ -26,7 +26,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       for i = #opts.ensure_installed, 1, -1 do
